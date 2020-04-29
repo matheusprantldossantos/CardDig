6f3793bdfb538ec22c24e7a46f3e36deb58786ea
matheusprantldossantos <49911159+matheusprantldossantos@users.noreply.github.com>
2020-04-29 02:14:17 -0300

    Tela de Pedido Mostrando de cada categoria

--- js/telaPedidos.js
+++ js/telaPedidos.js
@@ -2,23 +2,106 @@ $(new Document).ready(function(){
     $("#bBebidas").click(function(){
         fListaBebidas();
     });
+    $("#bPizza").click(function(){
+        fListaPizzas();
+    });
+    $("#bHamb").click(function(){
+        fListaHamburger();
+    });
+    $("#bSobremesa").click(function(){
+        fListaSobremesas();
+    });
+    $("#bLanche").click(function(){
+        fListaLanches();
+    });
+    
 });
+function fListaHamburger(){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "../php/hamburguerListar.php",
+        success : function(info, c){
+            var conteudo = "";
 
+            conteudo+="<div id='nomePag'>" + " HAMBURGUER " + "</div>" +
+                    "<div id='linha2'>" + "</div>";
+            conteudo += "<div id='subNome'>" + "Carne" + "</div>";
+            conteudo +="<div id='nomeProd'>" + "Nome: "+ info[0].nome + "</div>";
+            conteudo +="<div id='descProd'>" + "Preço: " +  info[0].preco + "</div>";
+
+                    $("#divPedidos").html(conteudo);
+        }
+    });
+}
+function fListaLanches(){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "../php/lanchesListar.php",
+        success : function(info, c){
+            var conteudo = "";
+
+            conteudo+="<div id='nomePag'>" + " LANCHES " + "</div>" +
+                    "<div id='linha2'>" + "</div>";
+            conteudo += "<div id='subNome'>" + "Saudável" + "</div>";
+            conteudo +="<div id='nomeProd'>" + "Nome: "+ info[0].nome + "</div>";
+            conteudo +="<div id='descProd'>" + "Preço: " +  info[0].preco + "</div>";
+
+                    $("#divPedidos").html(conteudo);
+        }
+    });
+}
+function fListaPizzas(){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "../php/pizzaListar.php",
+        success : function(info, c){
+            var conteudo = "";
+
+            conteudo+="<div id='nomePag'>" + " PIZZAS " + "</div>" +
+                    "<div id='linha2'>" + "</div>";
+            conteudo += "<div id='subNome'>" + "Salgadas" + "</div>";
+            conteudo +="<div id='nomeProd'>" + "Nome: "+ info[0].nome + "</div>";
+            conteudo +="<div id='descProd'>" + "Preço: " +  info[0].preco + "</div>";
+
+                    $("#divPedidos").html(conteudo);
+        }
+    });
+}
 function fListaBebidas(){
     $.ajax({
         type: "POST",
         dataType: "json",
-        url: "../php/prodListar.php",
-        success : function(info){
+        url: "../php/bebidaListar.php",
+        success : function(info, c){
             var conteudo = "";
 
             conteudo+="<div id='nomePag'>" + " BEBIDAS " + "</div>" +
-                    "<div id='linha2'>" + "</div>"
-                    "<div id='subNome'>" + "Bebidas Quentes" + "</div>" +
-                    "<div id='nomeProd'>" + info[0].preco + "</div>" +
-                    "<div id='descProd'>" + info[0].nome + "</div>";    
+                    "<div id='linha2'>" + "</div>";
+            conteudo += "<div id='subNome'>" + "Bebidas Quentes" + "</div>";
+            conteudo +="<div id='nomeProd'>" + "Nome: "+ info[0].nome + "</div>";
+            conteudo +="<div id='descProd'>" + "Preço: " +  info[0].preco + "</div>";
 
                     $("#divPedidos").html(conteudo);
         }
     }); // ajax
 } // fLista
+function fListaSobremesas(){
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "../php/sobremesasListar.php",
+        success : function(info, c){
+            var conteudo = "";
+
+            conteudo+="<div id='nomePag'>" + " SOBREMESAS " + "</div>" +
+                    "<div id='linha2'>" + "</div>";
+            conteudo += "<div id='subNome'>" + "Doces" + "</div>";
+            conteudo +="<div id='nomeProd'>" + "Nome: "+ info[0].nome + "</div>";
+            conteudo +="<div id='descProd'>" + "Preço: " +  info[0].preco + "</div>";
+                    $("#divPedidos").html(conteudo);
+        }
+    });
+}
