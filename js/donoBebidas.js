f3d5d2c722c8d712d0f8f7ae5e1409ef2dd3dab6
Alec-NK <55238706+Alec-NK@users.noreply.github.com>
2020-05-31 18:08:33 -0300

    botão Promoção

--- js/donoBebidas.js
+++ js/donoBebidas.js
@@ -21,6 +21,7 @@ function fListarCategoria(){
                 conteudo += "<div class='produtos' id='prodPre"+ i +"'>" + "R$ " + info[i].preco + "</div>";
                 conteudo += "<div class='produtos' id='prodTip"+ i +"'>" + info[i].tipo_categoria + "</div>";
                 conteudo += "<button class='buttEdit' id='prodEdit"+ i +"'>" + "<i class='far fa-edit'></i>" + "</button>";
+                conteudo += "<button class='buttPromo' id='prodPromo"+ i +"'>" + "<i class='fas fa-percent'></i>" + "</button>";
                 conteudo += "<button class='buttExc' id='prodExc"+ i +"'>" + "<i class='far fa-trash-alt'></i>" + "</button>";
                 conteudo += "<div class='linhaProd' id='line"+ i +"'>" + "</div>";
 
